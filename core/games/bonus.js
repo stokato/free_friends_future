61d71e9eb02e7d6232e8d5ff2e16054990bdad45
DasBit <dasbitbox@gmail.com>
2015-08-21 10:52:48 +0300

    Добавлена игра Бонус

--- core/games/bonus.js
+++ core/games/bonus.js
@@ -2,3 +2,34 @@
  * The game "Bonus level"
  * Guess the card, win gold
  */
+ 
+//Массив карточек
+var cards = new Array(7); 
+ 
+//Максимум м минимум для генератора случайных чисел
+var max = 99,
+    min = 0;
+  
+var temp  = 0, //Хранит максимальное значение массива
+    index = 0; //Хранит индекс максимального значения массива
+    
+function generatePot() {
+    
+    for(i = 0; i < cards.length; i++) {
+     cards[i] = (Math.random() * (max - min) + min).toFixed(0);
+    }
+
+    temp = cards[0];
+    
+    for(i = 0; i < cards.length; i++) {
+    if(temp < cards[i]) {
+        temp = cards[i];
+        index = i;
+    }
+    else continue;
+    }
+    
+    return index;
+}
+
+module.exports = generatePot;

c30eb26b4f24aef4761aa79a10db953a8301da5e
DasBit <dasbitbox@gmail.com>
2015-09-03 15:19:32 +0300

    Внесены правки в функцию addPlayer

--- core/player/index.js
+++ core/player/index.js
@@ -2,27 +2,50 @@ var Hapi      = require('hapi'),
     Cassandra = require('cassandra-driver');
 
 var cfg   = require('../config'),
-    query = require('../query');
+    query = require('../query'),
+    OS    = require('os');
 
 var server = new Hapi.Server();
     server.connection({port: cfg.WebPort, host: cfg.WebHost});
     
 var client = new Cassandra.Client({contactPoints: [cfg.DataHost,cfg.DataPort], keyspace: cfg.DataKeys});
-var points = 0;
     
 var player = {
     
-    addPlayer: function addPlayer(request){
+    addPlayer: function addPlayer(request, reply){
+        
+        //Задаем значение поинтов
+        var points = 0;
+        
+        //Путь к каталогу с фотками/авами
+        avatar = OS.tmpdir() + '\\img\\noavatar.jpg';
+        
+        //Сохраняем в переменные пароли из полей
+        //для последующей проверки на совпадение
+        var passwd    = request.payload.pass;
+        var re_passwd = request.payload.re_pass;
+        
+        //Разбиваем строку с датой рождения в массив
+        var data = request.payload.born_date;
+            data = data.split(".");
+            
+        //Сохранеям данные в отдельные переменные    
+        var D = data[0], //День
+            M = data[1], //Месяц
+            Y = data[2]; //Год
+        
+        if(passwd == re_passwd) {
         client.execute(query.addPlayer, 
-        [request.payload.ID, request.payload.first_name, request.payload.last_name, request.payload.login, request.payload.pass, request.payload.city, request.payload.born_date, request.payload.sex, request.payload.relation, request.params.avatar, points], 
-        {prepare: true}, function(err, result) {
+        [request.payload.ID, request.payload.first_name, request.payload.last_name, request.payload.login, request.payload.pass, request.payload.city, new Date(Y, M, D), request.payload.sex, request.payload.relation, avatar, points], 
+        {prepare: true}, function(err) {
             if(err) {
                 console.log(err);
             }
-            else {
-                console.log('Done: ', result);
-            }
-    });
+        });
+        }
+        else {
+            reply.redirect('/registration');
+        }
     }
 };
 
--- core/routes/index.js
+++ core/routes/index.js
@@ -1,15 +1,14 @@
 var Hapi = require('hapi');
+var fs = require('fs');
 
 var cfg    = require('../config'),
     player = require('../player');
-
-
+    
 var server = new Hapi.Server();
     server.connection({port: cfg.WebPort, host: cfg.WebHost});
     
 var routes = [
-      {method: 'POST', path:'/registration', handler:  function(request, reply) {reply(request.payload.Photo);}}, 
-    //{method: 'POST', path:'/registration', handler:  player.addPlayer}, 
+    {method: 'POST', path:'/registration', handler:  player.addPlayer}, 
     
     //Вьюшки
     {method: 'GET', path:'/', handler: { view: 'index.html'}},

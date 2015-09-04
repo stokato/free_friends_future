feeee0c2cb4266cdeaab02dc3c42ac425e7c2e39
DasBit <dasbitbox@gmail.com>
2015-09-04 12:30:41 +0300

    Добавлена вьюшка игрового стола

--- core/routes/index.js
+++ core/routes/index.js
@@ -8,10 +8,12 @@ var server = new Hapi.Server();
     server.connection({port: cfg.WebPort, host: cfg.WebHost});
     
 var routes = [
+    {method: 'POST', path:'/auth', handler: player.logon}, 
     {method: 'POST', path:'/registration', handler:  player.addPlayer}, 
     
     //Вьюшки
     {method: 'GET', path:'/', handler: { view: 'index.html'}},
+    {method: 'GET', path:'/table', handler: { view: 'table.html'}},
     {method: 'GET', path:'/registration', handler: { view: 'reg.html'}},
 ];
 

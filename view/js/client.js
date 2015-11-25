c094ebddbd9840f0738ce19c9038d682270447da
DasBit <dasbitbox@gmail.com>
2015-11-25 19:02:01 +0300

    Продолжается работа над чатом

--- view/js/client.js
+++ view/js/client.js
@@ -10,7 +10,9 @@ $( document ).ready(function() {
 		   Пока что можно использовать приставку id_ в паре со случайным числом
 		   в диапазоне от 1 до 99 */
 		   
-		var name = 'id_' + Math.random() * (99 - 1) + 1;
+		var name = Cookies.get('id_' + Math.random() * (99 - 1) + 1);
+		Cookies.set('name', name)
+		$('#message').focus();
 		return name;
 	}
 	
@@ -35,6 +37,44 @@ $( document ).ready(function() {
 		Time = H + ':' + M + ':' + S;
 		return Time;
 	}
+	
+	//Рендерим сообщения
+	function renderMessage(msg) {
+		msg = JSON.parse(msg);
+		var html = "<li>";
+		html += "<small class='time'>" + getTime(msg.t)  + " </small>";
+		html += "<span class='name'>" + msg.n + ": </span>";
+		html += "<span class='msg'>"  + msg.m + "</span>";
+		html += "</li>";
+		$('#messages').append(html);  
+	    return;
+    }
+	
+	
+	$('form').submit(function() {
+    
+	//Если поле ввода пустое, или в нем просто пробелы
+	//Сообщение отправлено не будет
+    if($('#message').val().match(/^[\s]*$/) !== null) { 
+       $('#message').val('');
+       $('#message').attr('placeholder', 'Введите свое сообщение');
+	   return false; 
+    }
+    
+    if(!Cookies.get('name') || Cookies.get('name').length < 1 || Cookies.get('name') === 'null') {
+      getName();
+      return false;
+    } else {
+      var msg  = $('#message').val();
+      socket.emit('io:message', msg);
+	  //Очищаем поле ввода для нового сообщения
+      $('#message').val(''); 
+	  //Чистим placeholder если сообщение успешно отправлено
+      $('#message').attr('placeholder', ''); 
+      return false;
+    }
+  });
+		
 	
 	
 });

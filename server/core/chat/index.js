90111a7df26692370259b31ac9c8f5445591d52c
DasBit <dasbitbox@gmail.com>
2015-12-11 14:05:17 +0300

    Прикрутил приватку

--- server/core/chat/index.js
+++ server/core/chat/index.js
@@ -8,14 +8,27 @@ var people = {};
 io.on('connection', function(socket) {
   console.log('connect');
   socket.broadcast.emit('user connected', 'К нам пришел' + socket.id);
-  socket.on('sendMessage', function(msg) {
+  
+socket.on('sendMessage', function(msg) {
     console.log(msg);
-    socket.emit('chat', {msg:msg.text, id:socket.id}); // врзвращаю себе сообщения 
-    socket.broadcast.emit('chat', {msg:msg.text, id:socket.id});// врзвращаю  остальним людям сообщения 
-  });
-  socket.on('disconnect', function() {
+    // возвращаю себе сообщения
+    socket.emit('chat', {msg:msg.text, id:socket.id});  
+    // возвращаю  остальним людям сообщения
+    socket.broadcast.emit('chat', {msg:msg.text, id:socket.id}); 
+});
+
+// Приватные сообщения
+socket.on('private message', function(msg, socket_id) {
+    // Возвращаю сам себе сообщение
+    socket.emit('chat', {msg:msg.text, id:socket.id});
+    //Отправить конкретному пользователю личным сообщением
+    io.sockets.socket(socket_id).emit('chat', {msg:msg.text, id:socket.id});
+});
+  
+socket.on('disconnect', function() {
     console.log('disconnect');
     socket.broadcast.emit('user disconnect', 'Покинул комнату' + socket.id);
     delete people[socket.id];
-  });
+});
+  
 });

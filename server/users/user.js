598cb2ea257a9718f1639078a1c60d81971ddfab
DasBit <DasBitBox@gmail.com>
2016-01-12 15:35:29 +0300

    Добавлен метод setProfile для модуля User

--- server/users/user.js
+++ server/users/user.js
@@ -9,3 +9,20 @@ function User() {
 	this.room = null;
 	this.seat = -1;
 }
+
+//Установка параметров профиля пользователя
+User.prototype.setProfile = function( settings ) {
+	this.uid = settings.uid;
+	this.profile = {
+		uid: settings.uid, //id пользователя
+		status: settings.status, //статус пользователя
+		name: settings.name, //имя пользователя
+		city: settings.city, //город пользователя
+		age: settings.age, //возраст пользователя
+		relation: settings.relation, //семейное положение пользователя
+		avatar: settings.avatar, //аватар пользователя
+		coins: settings.coins //монеты пользователя
+	};
+	
+	return this;
+};
